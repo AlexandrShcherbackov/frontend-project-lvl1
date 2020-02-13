5327bd7d1120123f9c52fe6457a6c3389b033fcf
AlexScherbackov <alsherbakov13@gmail.com>
2020-02-13 15:55:49 +0400

    fr-lvl-1-0002 added calc game

---
+++ src/games/calc.js
@@ -0,0 +1,20 @@
+import brainGames from '..';
+import { randomInt } from '../helpers/numbers';
+
+const gameDescription = "What is the result of the expression?";
+const operations = new Map([
+  ['+', (a, b) => (a + b)],
+  ['-', (a, b) => (a - b)],
+  ['*', (a, b) => (a * b)],
+]);
+
+const operatoinSymbols = ['+', '-', '*'];
+
+const calcGameData = () => {
+  const [fistOperand, secondOperand] = [randomInt(), randomInt()];
+  const operation = operatoinSymbols[randomInt(0, 2)];
+  const answer = (operations.get(operation))(fistOperand, secondOperand);
+  return [`${fistOperand} ${operation} ${secondOperand}`, `${answer}`];
+}
+
+export default () => (brainGames(gameDescription, calcGameData));
