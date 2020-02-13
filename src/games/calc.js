f7037f58a0dcd14a2cecb686e2e98942bf5ccf9a
AlexScherbackov <alsherbakov13@gmail.com>
2020-02-13 16:05:28 +0400

    eslint

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,7 +1,7 @@
 import brainGames from '..';
 import { randomInt } from '../helpers/numbers';
 
-const gameDescription = "What is the result of the expression?";
+const gameDescription = 'What is the result of the expression?';
 const operations = new Map([
   ['+', (a, b) => (a + b)],
   ['-', (a, b) => (a - b)],
@@ -15,6 +15,6 @@ const calcGameData = () => {
   const operation = operatoinSymbols[randomInt(0, 2)];
   const answer = (operations.get(operation))(fistOperand, secondOperand);
   return [`${fistOperand} ${operation} ${secondOperand}`, `${answer}`];
-}
+};
 
 export default () => (brainGames(gameDescription, calcGameData));
