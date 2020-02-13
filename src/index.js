365e0f0ba8181f018dc954cc6f676aeae1108604
AlexScherbackov <alsherbakov13@gmail.com>
2020-02-13 10:43:34 +0400

    fr-lvl-1-0001 first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+
+const actual = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${actual}!`);
